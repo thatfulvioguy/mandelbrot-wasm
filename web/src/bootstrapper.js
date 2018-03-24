baaf9b1005e0a237b4f521b64f8cd07d02069c0a
Fulvio Valente <2515227+thatfulvioguy@users.noreply.github.com>
2018-03-24 21:07:24 +0000

    Document why the bootstrapper exists and tweak the compatibility error message

--- web/src/bootstrapper.js
+++ web/src/bootstrapper.js
@@ -1,17 +1,23 @@
 
+// This bootstrapper checks for compatibility before we know it's ok to load the rest of the app.
+// Crucially, we use no recent language features here so this phase will work even on very old browsers.
+
 if (typeof WebAssembly !== 'object') {
   document.getElementById('root').insertAdjacentHTML('afterbegin', `
     <div>
       <p>This uses WebAssembly, but your browser doesn&apos;t seem to support it.</p>
       <p>
         Please make sure you&apos;re using an up to date version of a web browser that would support it, like Firefox,
-        Chrome or Safari. Even the mobile versions should be fine.
+        Chrome or Safari. The mobile versions should also be fine.
       </p>
     </div>
   `)
 } else {
+  // If we have wasm support, then we can assume the promise support necessary for the dynamic import polyfilling
+
   import('./main')
     .then(main => {
       main.start()
     })
+  // TODO handle errors
 }
