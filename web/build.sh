cc9a1eee9810c6bbea78d7f9a13cd4e8a137098a
Fulvio Valente <2515227+thatfulvioguy@users.noreply.github.com>
2018-04-20 17:42:26 +0100

    Bump some web deps

--- web/build.sh
+++ web/build.sh
@@ -6,7 +6,6 @@ rm -rf dist/*
 webpack --mode production
 cp src/index.html dist/
 
-# TODO do we need to use nightly or not? use a rust-toolchain file if so
 cargo build -p mandelbrot-wasm --release --target wasm32-unknown-unknown
 cp ../target/wasm32-unknown-unknown/release/mandelbrot_wasm.wasm dist/
 wasm-gc dist/*.wasm
